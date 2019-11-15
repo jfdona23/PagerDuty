460d14df0cd161ed13da4fbc8403c5131a81b8d8
Juan Fernando Donadío <jf.donadio@gmail.com>
2019-11-14 22:03:03 -0300

    some cleaning

--- auto_ack_pd.py
+++ auto_ack_pd.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
 #
-# Pull a triggered incident and push it as acknowledged.
+# Pull a triggered incident and push it as acknowledged or resolved.
 #
-# Manage incidents in PagerDuty with my user token (User API Key)
+# Endpoint to manage incidents in PagerDuty with user token (User API Key)
 # https://api.pagerduty.com/incidents
 #
-# Manage incidents in PagerDuty with Integration API Key
+# Endpoint to manage incidents in PagerDuty with Integration API Key
 # https://events.pagerduty.com/v2/enqueue
 
 
-from time import sleep
 from requests import request
 from json import loads, dumps
 
 
-zpd_token = 'YOUR_TOKEN'
+pd_token = 'YOUR TOKEN HERE'
 pd_url = 'https://api.pagerduty.com/incidents'
-pd_user = 'YOUR_USERID'
-
+pd_user = 'YOUR USER ID'
+pd_email = 'YOUR PD USER EMAIL' # User's email
+pd_email_alt = 'ALTERNATIVE PD EMAIL' # Alternative email in case you need to page yourself
+pd_service = 'YOUR PD SERVICE' # Service to send the page. Valid for send_page function (Not used...yet)
+tz = 'America/Los_Angeles' # Timezone to display the incidents
 
 def get_url(method, url, **kwargs):
     r = request(method, url, **kwargs)
-    j = r.json() 
+    j = r.json()
     return j
 
 
@@ -29,13 +31,13 @@ def get_page():
     pd_load = {
             'statuses[]': ['triggered'],
             'user_ids[]': [pd_user],
-            'time_zone' : 'America/Los_Angeles'
+            'time_zone' : tz
     }
 
     pd_headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/vnd.pagerduty+json;version=2',
-        'Authorization': 'Token token={0}'.format(zpd_token),
+        'Authorization': 'Token token={0}'.format(pd_token),
     }
     r = get_url('GET', pd_url, params=pd_load, headers=pd_headers)
     return r
@@ -46,7 +48,7 @@ def send_page(title):
       "incident": {
         "title": title,
         "service": {
-          "id": "P4XLKS1",
+          "id": pd_service,
           "type": "service_reference"
         },
         "assignments": [{
@@ -61,8 +63,8 @@ def send_page(title):
     pd_headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/vnd.pagerduty+json;version=2',
-        'From': 'YOUR_EMAIL',
-        'Authorization': 'Token token={0}'.format(zpd_token),
+        'From': '{0}'.format(pd_email_alt),
+        'Authorization': 'Token token={0}'.format(pd_token),
     }
 
     r = get_url('POST', pd_url, data=dumps(pd_load), headers=pd_headers)
@@ -83,17 +85,20 @@ def mod_page(id, status):
     pd_headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/vnd.pagerduty+json;version=2',
-        'From': 'YOUR_EMAIL',
-        'Authorization': 'Token token={0}'.format(zpd_token),
+        'From': '{0}'.format(pd_email),
+        'Authorization': 'Token token={0}'.format(pd_token),
     }
 
     r = get_url('PUT', pd_url, data=dumps(pd_load), headers=pd_headers)
     return r
 
 
-c = get_page()
+def main():
+    c = get_page() # Retrieve the current incidents
 
-for i in range(0,len(c['incidents'])):
-    id = c['incidents'][i]['id']
-    print("ACK to {0}".format(id))
-    mod_page(id,"acknowledged")
+    for i in range(0,len(c['incidents'])):
+        id = c['incidents'][i]['id']
+        print("ACK to {0}".format(id))
+        mod_page(id,"acknowledged")
+
+main()
